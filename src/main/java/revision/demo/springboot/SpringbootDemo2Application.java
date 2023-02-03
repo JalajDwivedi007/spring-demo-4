d44c938c6983bf5e3cc15a2baa8556df1c0335a2
JalajDwivedi007 <109264693+JalajDwivedi007@users.noreply.github.com>
2023-02-03 14:22:15 +0530

    Update SpringbootDemo2Application.java

--- src/main/java/revision/demo/springboot/SpringbootDemo2Application.java
+++ src/main/java/revision/demo/springboot/SpringbootDemo2Application.java
@@ -2,6 +2,7 @@ package revision.demo.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootDemo2Application {
